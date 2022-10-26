dd3b349166bb1cda6725e80df1972666aa417f17
MurtiC <murtiC95@hotmail.de>
2022-10-26 02:38:48 +0200

    Latex Labels added

--- charts.py
+++ charts.py
@@ -8,10 +8,6 @@ from matplotlib.figure import Figure
 from matplotlib import rc
 from matplotlib.backends.backend_pdf import PdfPages
 
-
-#data = pd.read_csv("data.csv",delimiter=';',header=0)
-
-
 cm = 1/2.54
 
 def chart(df, array,value,wh):
@@ -19,28 +15,27 @@ def chart(df, array,value,wh):
     plt.rc('legend', frameon=False)
     x = df.loc[:, array[0]]
     y = df.loc[:, array[1]]
-    # fig = plt.figure(figsize=(wh[0] * cm, wh[1]* cm))
     fig, ax = plt.subplots(figsize=(wh[0] * cm, wh[1] * cm), tight_layout=True)
-    # fig.set_dpi(value)
     fig.set_dpi(value)
     ax.plot(x, y, label=r'$Test Label$', color='red')
-    # ax.plot(x, y, label=r'$\forsen$!',color='red')
-    ax.set_xlabel(array[0])
-    ax.set_ylabel(array[1])
+    ax.set_xlabel(r'$'+array[0]+'$')
+    ax.set_ylabel(r'$'+array[1]+'$')
     ax.set_title("Test Title")
     ax.legend(loc="upper left")
-    # plt.xlabel(array[0])
-    # plt.ylabel(array[1])
+
 def scatter(df,array,value,wh):
     rc('text', usetex=True)
     plt.rc('legend', frameon=False)
     x = df.loc[:, array[0]]
     y = df.loc[:, array[1]]
-    fig = plt.figure(figsize=(wh[0] * cm, wh[1] * cm))
+    fig, ax = plt.subplots(figsize=(wh[0] * cm, wh[1] * cm), tight_layout=True)
     fig.set_dpi(value)
-    plt.xlabel(array[0])
-    plt.ylabel(array[1])
-    plt.scatter(x, y,color='red')
+    ax.scatter(x, y, label=r'$Test Label$',color='red')
+    ax.set_xlabel(r'$' + array[0] + '$')
+    ax.set_ylabel(r'$' + array[1] + '$')
+    ax.set_title("Test Title")
+    ax.legend(loc="upper left")
+
 
 def hist(df,array,value,wh):
     rc('text', usetex=True)
