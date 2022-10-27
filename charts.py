24fb94ee1f074aa8bfdd974b176ca6b42c3183e1
MurtiC <murtiC95@hotmail.de>
2022-10-27 11:33:23 +0200

    replaced sliders with entries, also cleanup

--- charts.py
+++ charts.py
@@ -7,44 +7,59 @@ import csv
 from matplotlib.figure import Figure
 from matplotlib import rc
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from gui import *
 
 cm = 1/2.54
 
-def chart(df, array,value,wh):
+def chart(df,array,win):
+
     rc('text', usetex=True)
     plt.rc('legend', frameon=False)
     x = df.loc[:, array[0]]
     y = df.loc[:, array[1]]
-    fig, ax = plt.subplots(figsize=(wh[0] * cm, wh[1] * cm), tight_layout=True)
-    fig.set_dpi(value)
+    fig, ax = plt.subplots(figsize=(array[2] * cm, array[3] * cm), tight_layout=True)
+    fig.set_dpi(array[4])
     ax.plot(x, y, label=r'$Test Label$', color='red')
     ax.set_xlabel(r'$'+array[0]+'$')
     ax.set_ylabel(r'$'+array[1]+'$')
     ax.set_title("Test Title")
     ax.legend(loc="upper left")
+    canvas = FigureCanvasTkAgg(fig,master=win)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=8, column=0, padx=5, pady=5)
+    canvas.get_tk_widget().grid(row=9, column=0, padx=5, pady=5)
 
-def scatter(df,array,value,wh):
+def scatter(df,array,win):
     rc('text', usetex=True)
     plt.rc('legend', frameon=False)
     x = df.loc[:, array[0]]
     y = df.loc[:, array[1]]
-    fig, ax = plt.subplots(figsize=(wh[0] * cm, wh[1] * cm), tight_layout=True)
-    fig.set_dpi(value)
+    fig, ax = plt.subplots(figsize=(array[2] * cm, array[3] * cm), tight_layout=True)
+    fig.set_dpi(array[4])
     ax.scatter(x, y, label=r'$Test Label$',color='red')
     ax.set_xlabel(r'$' + array[0] + '$')
     ax.set_ylabel(r'$' + array[1] + '$')
     ax.set_title("Test Title")
     ax.legend(loc="upper left")
+    canvas = FigureCanvasTkAgg(fig, master=win)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=8, column=0, padx=5, pady=5)
+    canvas.get_tk_widget().grid(row=9, column=0, padx=5, pady=5)
 
-
-def hist(df,array,value,wh):
+def hist(df,array,win):
     rc('text', usetex=True)
     plt.rc('legend', frameon=False)
     x = df.loc[:, array[0]]
     y = df.loc[:, array[1]]
-    fig = plt.figure(figsize=(wh[0] * cm, wh[1] * cm))
-    fig.set_dpi(value)
+    fig = plt.figure(figsize=(array[2] * cm, array[3] * cm))
+    fig.set_dpi(array[4])
     plt.xlabel(array[0])
     plt.ylabel(array[1])
     plt.hist(y,bins = 'auto',color='red',alpha=0.7, rwidth=0.85)
+    canvas = FigureCanvasTkAgg(fig, master=win)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=8, column=0, padx=5, pady=5)
+    canvas.get_tk_widget().grid(row=9, column=0, padx=5, pady=5)
+
 
--- gui.py
+++ gui.py
@@ -14,40 +14,23 @@ from tkinter import filedialog
 import os
 
 
-
-
-
 def count():
     x = len(plt.get_fignums())
     plotcount.config(text = x)
-
-def sel():
-
-    xscale = str(width.get())
-    yscale = str(height.get())
-    x = int(float(xscale))
-    y = int(float(yscale))
-    wh = [x,y]
-    return wh
-    #scalelabel.config(text = z)
-
-
 
 def setxy():
 
     x = xclicked.get()
     y = yclicked.get()
-    z = [x,y]
+    xnum = float(xnumber.get())
+    ynum = float(ynumber.get())
+    dpi = int(entry.get())
+    z = [x,y,xnum,ynum,dpi]
     return z
-
-def setdpi():
-    y = int(entry.get())
-    return y
 
 def open_win_diag():
    # Create a dialog box
    path=filedialog.askopenfilename(initialdir=os.getcwd())
-   print(path)
    file = pd.read_csv(path, delimiter=';', header=0)
    return file
 
@@ -55,26 +38,22 @@ def update():
     win.destroy()
     os.system('python main.py')
 
-
-
 def output(value):
-    x = len(plt.get_fignums())
 
+    x = len(plt.get_fignums())
     #filename = "output"
     #pp = PdfPages(filename)
     fig_nums = plt.get_fignums()
     figs = [plt.figure(n) for n in fig_nums]
     for fig in figs:
-        fig.set_dpi(value)
-
+        fig.set_dpi(value[4])
     #    fig.savefig(pp, format='pdf')
-
     #pp.close()
     plt.show()
 
 def selectplot():
     x = plotoption.get()
-    return eval(x +"(data,setxy(),setdpi(),sel())")
+    return eval(x +"(data,setxy(),win)")
 
 
 data = open_win_diag()
@@ -88,13 +67,10 @@ options = data.columns.tolist()
 
 xclicked = tk.StringVar()
 yclicked = tk.StringVar()
-
 yclicked.set("Y axis")
 xclicked.set("X axis")
 
 plotoption = tk.StringVar()
-
-
 
 dpiwert = tk.IntVar()
 varx = IntVar()
@@ -116,9 +92,6 @@ plot_bar.grid(row=4, column=0, padx=5, pady=5)
 
 
 
-
-
-
 # Drop Down Menu
 dropx = ttk.OptionMenu(xy, xclicked, options[0], *options)
 dropx.grid(row = 0, column= 0,padx=50, pady=0)
@@ -127,32 +100,17 @@ dropy.grid(row = 0, column= 1,padx=50, pady=0)
 dropf = ttk.OptionMenu(top, plotoption,plotnames[0],*plotnames)
 dropf.grid(row = 1, column= 1,padx=50, pady=0)
 
-
-width = Scale(xy,from_= 1, to=30, orient=HORIZONTAL, variable= varx)
-width.grid(row = 1,column= 0,padx=50, pady=10)
-height = Scale(xy,from_= 1, to=30, orient=HORIZONTAL, variable= vary)
-height.grid(row = 1,column= 1,padx=50, pady=10)
-
-#scalebtn = Button(top, text="Get Scale Value", command= sel)
-#scalebtn.grid(row = 0, column= 0,padx=50, pady=0)
-
-#scalelabel = Label(top)
-#scalelabel.grid(row = 0, column= 4,padx=50, pady=0)
+# Set Length of the xy axis
+xnumber = ttk.Entry(xy)
+xnumber.insert(END, '16')
+xnumber.grid(row = 1,column= 0,padx=50, pady=10)
+ynumber = ttk.Entry(xy)
+ynumber.insert(END, '9')
+ynumber.grid(row = 1,column= 1,padx=50, pady=10)
 
 
-#btn1 = Button(top, text="Normaler Plot", command=lambda: plot_name.set("chart"))
-#btn1 = ttk.Button(top, text="Normaler Plot", command=lambda: chart(data,setxy(),setdpi(),sel()))
-#btn1.grid(row = 1, column= 0,padx=5, pady=5)
 btn1 = ttk.Button(top, text="Choose Plot", command=lambda: selectplot())
 btn1.grid(row = 1, column= 0,padx=5, pady=5)
-
-
-#btn2 = Button(top, text="Scatter Plot", command=lambda: plot_name.set("scatter"))
-#btn2 = ttk.Button(top, text="Scatter Plot", command=lambda: scatter(data,setxy(),setdpi(),sel()))
-#btn2.grid(row = 2, column= 0,padx=5, pady=5)
-#btn3 = Button(top, text="Hist Plot", command=lambda: plot_name.set("hist"))
-#btn3 = ttk.Button(top, text="Hist Plot", command=lambda: hist(data,setxy(),setdpi(),sel()))
-#btn3.grid(row = 3, column= 0,padx=5, pady=5)
 
 # Display the number of plots, optional
 button = ttk.Button(top, text= "Number of Plots",  command=lambda: count())
@@ -169,40 +127,13 @@ entry = ttk.Entry(top)
 entry.insert(END, '200')
 entry.grid(row = 5, column= 1,padx=0, pady=5)
 
-
-generate = ttk.Button(plot_bar, text="Generate", command=lambda: output(setdpi()))
+generate = ttk.Button(plot_bar, text="Generate", command=lambda: output(setxy()))
 generate.grid(row = 6, column= 0,padx=5, pady=5)
 
-change = ttk.Button(plot_bar, text="Change File", command=lambda: update())
+change = ttk.Button(plot_bar, text="Refresh", command=lambda: update())
 change.grid(row = 6, column= 1,padx=5, pady=5)
 
 exit = ttk.Button(plot_bar, text="Close Window", command= win.quit)
 exit.grid(row = 6, column= 3,padx=5, pady=5)
 
-
-#resetbtn= Button(plot_bar, text="Reset", command=lambda: reset())
-#resetbtn.grid(row = 6, column= 1,padx=5, pady=5)
-
-
-
-
-
-#btn1 = Button(plot_bar, text="Normaler Plot", command=lambda: simplechart(data, setxy()))
-#btn1.grid(row = 3, column= 0,padx=5, pady=5)
-#btn2 = Button(plot_bar, text="Scatter Plot", command=lambda: scatter(data, setxy()))
-#btn2.grid(row = 4, column= 0,padx=5, pady=5)
-#btn3 = Button(plot_bar, text="Hist Plot", command=lambda: hist(data, setxy()))
-#btn3.grid(row = 5, column= 0,padx=5, pady=5)
-
-################################
-
-fig = plt.Figure(figsize=(6,4), dpi=50)
-bar1 = FigureCanvasTkAgg(fig, win)
-bar1.get_tk_widget().grid(row=8, column=0, ipadx=40, ipady=20)
-canvas = FigureCanvasTkAgg(fig, master=win)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().grid(row=8, column=0, ipadx=40, ipady=20)
-toolbarFrame = Frame(master=win)
-toolbarFrame.grid(row=9,column=0)
-toolbar = NavigationToolbar2Tk(canvas, toolbarFrame)
 
